7588ec97cd53f76df5811da6ea538f23670fb7bb
Antônio Santana <antonionetosg6@gmail.com>
2022-09-09 13:31:47 -0300

    Requisito 5

--- src/Component/Source.js
+++ src/Component/Source.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Category from './Category';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Source extends React.Component {
   state = {
     category: [],
+    search: '',
+    productsList: [],
   };
 
   componentDidMount() {
@@ -22,14 +24,41 @@ class Source extends React.Component {
     return a;
   };
 
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      search: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { search } = this.state;
+    const DATA = await getProductsFromCategoryAndQuery(search, search);
+    const { results } = DATA;
+    this.setState({
+      productsList: results,
+    });
+  };
+
   render() {
+    const { productsList } = this.state;
+
     return (
       <div>
         <label htmlFor="search">
           <input
+            data-testid="query-input"
             name="search"
             type="text"
+            onChange={ this.handleChange }
           />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
           <h3 data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h3>
@@ -39,9 +68,23 @@ class Source extends React.Component {
           <ul>
             {this.creatCategory()}
           </ul>
+          <div>
+            {
+              productsList.length > 0
+                ? productsList.map((i) => (
+                  <div key={ i.id } data-testid="product">
+                    <h5>{i.title}</h5>
+                    <img src={ i.thumbnail } alt={ i.title } />
+                    <h6>{i.price}</h6>
+                  </div>
+                ))
+                : <h3>Nenhum produto foi encontrado</h3>
+            }
+          </div>
+
         </aside>
       </div>
- 
+
     );
   }
 }
