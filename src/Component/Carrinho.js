ef64ae416ad6bb26620bfd7cf5024c599339e02f
Thiago Reis Nascimento <trnasci@gmail.com>
2022-09-11 18:34:18 -0300

    Resolução do requisito 10

--- src/Component/Carrinho.js
+++ src/Component/Carrinho.js
@@ -29,20 +29,95 @@ export default class Carrinho extends Component {
     }
   }
 
+  handlerAddQty = (event) => {
+    const { id } = event.target;
+    const { productsToCart } = this.state;
+    const productFound = productsToCart.find((product) => (product.title === id));
+    if (productFound) {
+      productsToCart.forEach((product) => {
+        if (product.title === id) {
+          product.qty += 1;
+        }
+      });
+    }
+    localStorage.setItem('products', JSON.stringify(productsToCart));
+    let CART_PRODUCTS = null;
+    CART_PRODUCTS = JSON.parse(localStorage.getItem('products'));
+    this.setState({
+      productsToCart: CART_PRODUCTS,
+    });
+  };
+
+  handlerSubQty = (event) => {
+    const { id } = event.target;
+    const { productsToCart } = this.state;
+    const productFound = productsToCart.find((product) => (product.title === id));
+    if (productFound) {
+      productsToCart.forEach((product) => {
+        if (product.title === id && product.qty > 1) {
+          product.qty -= 1;
+        }
+      });
+    }
+    localStorage.setItem('products', JSON.stringify(productsToCart));
+    let CART_PRODUCTS = null;
+    CART_PRODUCTS = JSON.parse(localStorage.getItem('products'));
+    this.setState({
+      productsToCart: CART_PRODUCTS,
+    });
+  };
+
+  handlerRemoveProduct = (event) => {
+    const { id } = event.target;
+    const { productsToCart } = this.state;
+    const productFound = productsToCart.filter((product) => (product.title !== id));
+    localStorage.setItem('products', JSON.stringify(productFound));
+    let CART_PRODUCTS = null;
+    CART_PRODUCTS = JSON.parse(localStorage.getItem('products'));
+    this.setState({
+      productsToCart: CART_PRODUCTS,
+    });
+  };
+
   render() {
     const { productsToCart, emptyCart } = this.state;
     if (emptyCart) {
       return (
         <div>
+          <h2>Carrinho de compras</h2>
           {
             productsToCart.map((produto) => (
               <div key={ produto.title }>
+                <button
+                  data-testid="remove-product"
+                  type="submit"
+                  id={ produto.title }
+                  onClick={ this.handlerRemoveProduct }
+                >
+                  Remover produto
+                </button>
                 <h3 data-testid="shopping-cart-product-name">{produto.title}</h3>
-                <img src={ produto.thumnail } alt={ produto.title } />
+                <img src={ produto.thumbnail } alt={ produto.title } />
                 <h4>{`R$: ${produto.price}`}</h4>
+                <button
+                  data-testid="product-increase-quantity"
+                  type="submit"
+                  onClick={ this.handlerAddQty }
+                  id={ produto.title }
+                >
+                  +
+                </button>
                 <span>
                   <h4 data-testid="shopping-cart-product-quantity">{produto.qty}</h4>
                 </span>
+                <button
+                  data-testid="product-decrease-quantity"
+                  type="submit"
+                  id={ produto.title }
+                  onClick={ this.handlerSubQty }
+                >
+                  -
+                </button>
               </div>
             ))
           }
